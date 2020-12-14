390fad931ff9aaee58f9b4a3b9d0e91417c94c1d
kharchenkok <k.v.kharchenko@ukr.net>
2020-12-14 17:18:03 +0200

    team project added

---
+++ src/js/script_test.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  var owl = $('.test-list');
+  owl.owlCarousel({
+    items: 1,
+    dots: false,
+    dotsContainer: false,
+    navContainer: false
+  });
+
+  $('.btn-next').click(function () {
+    owl.trigger('next.owl.carousel');
+  })
+
+
+  // Go to the previous item
+  $('.btn-prev').click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+  });
+});
